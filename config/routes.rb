1455a4e3cf4ace7b7b3465de3706986c4255b029
luting pan <panluting.cn@gmail.com>
2020-08-05 14:20:34 +0800

    added create action for gigs

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :gigs, only: [ :index ]
+      resources :gigs, only: [ :index, :show, :create ]
     end
   end
 end
