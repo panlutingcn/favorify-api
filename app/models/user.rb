3124c121e0e7526b691c60d1e8f67796ebcf15c4
Marin Bargan <niramnagrab@gmail.com>
2020-08-06 16:05:36 +0800

    added login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :gigs, dependent: :destroy
   has_many :bids, dependent: :destroy
-  validates :name, presence: true
+  # validates :name, presence: true
 end
