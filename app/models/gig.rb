3d66a191499bc65a3db0e6e2ef2486ed67b1656d
Marin Bargan <niramnagrab@gmail.com>
2020-08-06 16:13:34 +0800

    added search

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -2,4 +2,12 @@ class Gig < ApplicationRecord
   belongs_to :user
   has_many :bids, dependent: :destroy
   validates :name, :address, :description, :price, presence: true
+
+  include PgSearch::Model
+
+  pg_search_scope :global_search,
+  against: [:name, :address, :description, :price],
+  using: {
+    tsearch: { prefix: true}
+  }
 end
